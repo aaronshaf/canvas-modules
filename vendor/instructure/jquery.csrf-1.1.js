714c7eece01616a6746ae1e8da2a1dd499c11ff8
Aaron Shafovaloff <ashafovaloff@instructure.com>
2013-08-03 11:52:19 -0600

    don't immediately execute

--- vendor/instructure/jquery.csrf-1.1.js
+++ vendor/instructure/jquery.csrf-1.1.js
@@ -1,18 +1,19 @@
-define('jquery',function($) {
-  var _parseJSON = $.parseJSON;
-
-  $.parseJSON = function() {
-    if (arguments[0]) {
-      try {
-        var newData = arguments[0].replace(/^while\(1\);/, '');
-        arguments[0] = newData;
-      } catch (err) {
-        // data was not a string or something, just pass along to the real parseJSON
-        // and let it handle errors.
+define(function() {
+  return function($) {
+    $.parseJSON = function() {
+      if (arguments[0]) {
+        try {
+          var newData = arguments[0].replace(/^while\(1\);/, '');
+          arguments[0] = newData;
+        } catch (err) {
+          // data was not a string or something, just pass along to the real parseJSON
+          // and let it handle errors.
+        }
       }
-    }
-    return _parseJSON.apply($, arguments);
-  };
+      return _parseJSON.apply($, arguments);
+    };
 
-  $.ajaxSettings.converters["text json"] = $.parseJSON;
+    $.ajaxSettings.converters["text json"] = $.parseJSON;
+    return $;
+  }
 });
