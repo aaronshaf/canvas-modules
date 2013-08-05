b45cbda86134270b8362bab9caac1bc1ea8e8ce7
Aaron Shafovaloff <ashafovaloff@instructure.com>
2013-08-05 17:52:16 -0600

    clear files

--- export.sh
+++ export.sh
@@ -1,3 +1,10 @@
+rm -r ../canvas-lms/app/coffeescripts/ember/context_modules2/config/
+rm -r ../canvas-lms/app/coffeescripts/ember/context_modules2/controllers/
+rm -r ../canvas-lms/app/coffeescripts/ember/context_modules2/models/
+rm -r ../canvas-lms/app/coffeescripts/ember/context_modules2/routes/
+rm -r ../canvas-lms/app/coffeescripts/ember/context_modules2/templates/
+rm -r ../canvas-lms/app/coffeescripts/ember/context_modules2/views/
+
 cp -R app/config ../canvas-lms/app/coffeescripts/ember/context_modules2/
 cp -R app/controllers ../canvas-lms/app/coffeescripts/ember/context_modules2/
 cp -R app/models ../canvas-lms/app/coffeescripts/ember/context_modules2/
