a6614f17d79ffdcb626bf0caac6eca27ee18c178
Aaron Shafovaloff <ashafovaloff@instructure.com>
2013-08-05 15:54:45 -0600

    temporary export script

---
+++ export.sh
@@ -0,0 +1,12 @@
+cp -R app/config ../canvas-lms/app/coffeescripts/ember/context_modules2/
+cp -R app/controllers ../canvas-lms/app/coffeescripts/ember/context_modules2/
+cp -R app/models ../canvas-lms/app/coffeescripts/ember/context_modules2/
+cp -R app/routes ../canvas-lms/app/coffeescripts/ember/context_modules2/
+cp -R app/templates ../canvas-lms/app/coffeescripts/ember/context_modules2/
+cp -R app/views ../canvas-lms/app/coffeescripts/ember/context_modules2/
+
+# sass
+
+# vendor?
+
+# img?
