73bf09cbee0f36a33afdcebc0733fb37e7977fb5
Aaron Shafovaloff <ashafovaloff@instructure.com>
2013-08-23 10:28:38 -0600

    export script wasn't including components, lib

--- export.sh
+++ export.sh
@@ -1,16 +1,13 @@
-rm -r ../canvas-lms/app/coffeescripts/ember/config/
-rm -r ../canvas-lms/app/coffeescripts/ember/controllers/
-rm -r ../canvas-lms/app/coffeescripts/ember/models/
-rm -r ../canvas-lms/app/coffeescripts/ember/routes/
-rm -r ../canvas-lms/app/coffeescripts/ember/templates/
-rm -r ../canvas-lms/app/coffeescripts/ember/views/
+rm -r ../canvas-lms/app/coffeescripts/ember/context_modules2/
 
-cp -R app/config ../canvas-lms/app/coffeescripts/ember/
-cp -R app/controllers ../canvas-lms/app/coffeescripts/ember/
-cp -R app/models ../canvas-lms/app/coffeescripts/ember/
-cp -R app/routes ../canvas-lms/app/coffeescripts/ember/
-cp -R app/templates ../canvas-lms/app/coffeescripts/ember/
-cp -R app/views ../canvas-lms/app/coffeescripts/ember/
+cp -R app/components ../canvas-lms/app/coffeescripts/ember/context_modules2/
+cp -R app/config ../canvas-lms/app/coffeescripts/ember/context_modules2/
+cp -R app/controllers ../canvas-lms/app/coffeescripts/ember/context_modules2/
+cp -R app/lib ../canvas-lms/app/coffeescripts/ember/context_modules2/
+cp -R app/models ../canvas-lms/app/coffeescripts/ember/context_modules2/
+cp -R app/routes ../canvas-lms/app/coffeescripts/ember/context_modules2/
+cp -R app/templates ../canvas-lms/app/coffeescripts/ember/context_modules2/
+cp -R app/views ../canvas-lms/app/coffeescripts/ember/context_modules2/
 
 # sass
 cp stylesheets/context_modules2.scss ../canvas-lms/app/stylesheets/
