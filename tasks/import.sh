aa844f02a966ebac7cc0a3e800ed86a9560e1446
Aaron Shafovaloff <ashafovaloff@instructure.com>
2013-09-20 16:12:53 -0600

    assume "canvas", not "canvas-lms" for cloned directory of canvas

--- tasks/import.sh
+++ tasks/import.sh
@@ -1,4 +1,4 @@
-echo "importing from ../canvas-lms/app/coffeescripts/ember/context_modules2/"
+echo "importing from ../canvas/app/coffeescripts/ember/context_modules2/"
 
 rm -r components
 rm -r config
@@ -10,19 +10,19 @@ rm -r routes
 rm -r templates
 rm -r views
 
-cp -R ../canvas-lms/app/coffeescripts/ember/context_modules2/components .
-cp -R ../canvas-lms/app/coffeescripts/ember/context_modules2/config .
-cp -R ../canvas-lms/app/coffeescripts/ember/context_modules2/controllers .
-cp -R ../canvas-lms/app/coffeescripts/ember/context_modules2/helpers .
-cp -R ../canvas-lms/app/coffeescripts/ember/context_modules2/lib .
-cp -R ../canvas-lms/app/coffeescripts/ember/context_modules2/models .
-cp -R ../canvas-lms/app/coffeescripts/ember/context_modules2/routes .
-cp -R ../canvas-lms/app/coffeescripts/ember/context_modules2/templates .
-cp -R ../canvas-lms/app/coffeescripts/ember/context_modules2/views .
-cp -R ../canvas-lms/app/coffeescripts/ember/context_modules2/main.coffee .
+cp -R ../canvas/app/coffeescripts/ember/context_modules2/components .
+cp -R ../canvas/app/coffeescripts/ember/context_modules2/config .
+cp -R ../canvas/app/coffeescripts/ember/context_modules2/controllers .
+cp -R ../canvas/app/coffeescripts/ember/context_modules2/helpers .
+cp -R ../canvas/app/coffeescripts/ember/context_modules2/lib .
+cp -R ../canvas/app/coffeescripts/ember/context_modules2/models .
+cp -R ../canvas/app/coffeescripts/ember/context_modules2/routes .
+cp -R ../canvas/app/coffeescripts/ember/context_modules2/templates .
+cp -R ../canvas/app/coffeescripts/ember/context_modules2/views .
+cp -R ../canvas/app/coffeescripts/ember/context_modules2/main.coffee .
 
 # sass
-cp ../canvas-lms/app/stylesheets/context_modules2.scss stylesheets/
+cp ../canvas/app/stylesheets/context_modules2.scss stylesheets/
 
 # tests
 
